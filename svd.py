49592382a70340948aca615d4990a1c7ee7eda82
Viktoriia Lushpak <lushpak.pn@ucu.edu.ua>
2025-04-05 17:10:54 +0300

    Added book classification for SVD

--- svd.py
+++ svd.py
@@ -1,6 +1,10 @@
 from tdm_generator import build_matrix
 import numpy as np
 import sys
+from transformers import pipeline
+import logging
+
+# logging.getLogger("transformers").setLevel(logging.ERROR)
 
 def svd(matrix):
     transpose = np.transpose(matrix)
@@ -38,6 +42,54 @@ def main():
     doc_names, vocabulary, term_doc_matrix = build_matrix(input_folder, stop_words, name_path)
 
     U, E, V = svd(term_doc_matrix)
+
+    classifier = pipeline("zero-shot-classification",
+                            model="facebook/bart-large-mnli")
+    
+    dimension_labels = []
+
+    candidate_labels = [
+        "fantasy",
+        "romance",
+        "science fiction",
+        "horror",
+        "thriller",
+        "adventure",
+        "drama",
+        "comedy",
+        "crime",
+        "psychology",
+        "philosophy",
+        "science",
+        "economics",
+        "politics"
+    ]
+
+    for d in range(V.shape[1]): 
+        dimension_weights = V[:, d]
+
+        top_indices = np.argsort(np.abs(dimension_weights))[::-1]
+        top_words = [vocabulary[i] for i in top_indices[:30]]
+        dimension_description = " ".join(top_words)
+
+        result = classifier(dimension_description, candidate_labels)
+
+        top3_indices = sorted(range(len(result["scores"])), key=lambda i: result["scores"][i], reverse=True)[:3]
+        top3_genres = [result["labels"][i] for i in top3_indices]
+        dimension_labels.append(top3_genres)
+        
+    doc_topic_vectors = U @ E
+
+    doc_to_topic = []
+
+    for i in range(V.shape[1]):
+        row_vec = doc_topic_vectors[i, :]
+        best_dim = np.argmax(np.abs(row_vec))
+        doc_topic = dimension_labels[best_dim]
+        doc_to_topic.append(doc_topic)
+    
+    for i, topic in enumerate(doc_to_topic):
+        print(f"{doc_names[i].strip('.txt')} => {topic}")
 
 if __name__ == "__main__":
     main()
